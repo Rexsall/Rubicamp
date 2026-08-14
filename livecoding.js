6b47e8063cfeb55f0e7af4344b3a114d555de382
rexsal <ecalcodett@gmail.com>
2026-08-14 13:16:43 +0700

    sql

---
+++ livecoding.js
@@ -0,0 +1,7 @@
+let umur = 30;
+// Tulis kode if statement di sini
+if (umur => 20)
+    console.log("Anda sudah dewasa");
+console.log("Anda bisa memilih dalam pemilu");
+
+console.log("Terima kasih");
